e53ce9894644d4280d40a1fc194e781a8595aaaf
Thibaut Mottet <thibaut.mottet@gmai.com>
2019-09-24 14:46:52 +0200

    first commit

---
+++ components/JHipsterLogo.vue
@@ -0,0 +1,14 @@
+<template>
+  <a href="https://developer.android.com" target="_blank">
+    <v-avatar tile>
+      <v-img src="https://tokbox.com/blog/wp-content/uploads/2017/05/Android-and-Kotlin.jpg"></v-img>
+    </v-avatar>
+  </a>
+</template>
+
+<script>
+    export default {
+        name: "AndroidLogo"
+    }
+</script>
+
---
+++ components/NuxtLogo.vue
@@ -0,0 +1,15 @@
+<template>
+  <a href="https://firebase.google.com" target="_blank">
+    <v-avatar tile>
+      <v-img
+        src="https://4.bp.blogspot.com/-Fxo_qnGJBj0/WRoDPNdlEII/AAAAAAAABF0/1mSHmv5gleQaCsHKEDgTB3DbNghjCXvZACLcB/s1600/logo_firebase_1920px_clr.png"></v-img>
+    </v-avatar>
+  </a>
+</template>
+
+<script>
+    export default {
+        name: "FirebaseLogo"
+    }
+</script>
+
